5de66cd9068360cce1bd39c8ec9dbd3aaecd86e4
JonathanHasASecondName <jonathanpoot3009@gmail.com>
2022-11-20 23:28:04 +0100

    Added ugly automatic graph reader

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+
+from os import path
+"""
+Contains relevant file paths for the program
+"""
+
+ROOT_DIR = path.realpath(path.join(path.dirname(__file__)))
+shear_factors_path = ROOT_DIR + "/shear_bearing_factors.xlsx"
+tension_factors_path = ROOT_DIR + "/tension_efficiency_factors.xlsx"
+
 # Forces applied to the entire assembly
 external_forces = [1, 1, 1]  # Newton, x, y and z
 external_moments = [0, 0, 0]  # Newton meter, x, y and z
