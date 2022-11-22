b95ea8cb077c7067cd75b37753fe7a607f48df14
AlexRadac <alexandruradac@gmail.com>
2022-11-22 15:52:06 +0100

    Pty functions

--- Oblique_Loading/P_ty_Function.py
+++ Oblique_Loading/P_ty_Function.py
@@ -1,26 +1,27 @@
 f_ty=float(480000000) #[Pa]
-diameter_1=float(2) #[cm]
-thickness_1=float(1) #[cm]
-width_1=float(4) #[cm]
+diameter_1=float(0.02) #[m]
+thickness_1=float(0.01) #[m]
+width_1=float(0.04) #[m]
 design_load=float(1000) #[N]
 
+def ratio_d_15(f_ty,diameter_1,thickness_1,width_1,design_load):
+    a_1=a_4=float(width_1/2-(0.7071*diameter_1/2))
+    a_2=a_3=float(width_1/2-diameter_1/2)
+    a_av=float(6/(3/a_1+1/a_2+1/a_3+1/a_4))
+    a_br=float(diameter_1*thickness_1)
+    ratio=float(a_av/a_br)
 
-a_1=a_4=width_1/2-(0.7071*diameter_1/2)
-a_2=a_3=width_1/2-diameter_1/2
-a_av=6/(3/a_1+1/a_2+1/a_3+1/a_4)
+    return ratio
 
-a_br=diameter_1*thickness_1
-ratio=a_av/a_br
-print(ratio)
-print(a_br)
-print(a_av)
+def r_transverse(a_br,f_ty,design_load):
+    k_ty=graph_reader(ratio)
+    p_ty=float(k_ty*a_br*f_ty)
+    ms=float((p_ty/design_load)-1)
+    r_tr=float(design_load/p_ty)
 
-k_ty=1.18
+    return ms,r_tr
 
-p_ty=k_ty*a_br*f_ty*0.0001
-ms=(p_ty/design_load)-1
-r_tr=design_load/p_ty
 
-print(ms)
-print(p_ty)
-print(r_tr)
+
+
+
