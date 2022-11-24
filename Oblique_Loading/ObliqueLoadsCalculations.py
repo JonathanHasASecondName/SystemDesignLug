4f531584256ef3ef4dd3e8166cfe01807461fbb4
hihihellohihi3998 <mateo.arganza@gmail.com>
2022-11-24 15:00:00 +0100

    1. Added axial load chart reading functionality to graph reader. 2. Included filepath for axial load in config.py 3. Added excel file for axial load so everyone has access 4. Addded graph reader input to oblique load calculations

--- Oblique_Loading/ObliqueLoadsCalculations.py
+++ Oblique_Loading/ObliqueLoadsCalculations.py
@@ -1,18 +1,19 @@
 import pandas as pd
 from graph_reader import graph_reader
 #Input values
-diameter_1 = 0.001
+diameter_1 = 0.02
 thickness_1 = 0.001
-width_1 = 0.002
-stress_yield = 1000
-stress_ultimate = 1000
+width_1 = 0.0
+stress_yield = 1000*10**6
+stress_ultimate = 1000*10**6
 density = 2800
 F_z = 1000
 F_y = 0.1 * F_z
 
 #Calculated parameters
-A_1=A_4=width_1/2-(0.7071*diameter_1/2)
-A_2=A_3=width_1/2-diameter_1/2
+A_1=(width_1/2-(0.7071*diameter_1/2))*thickness_1
+A_4=A_1
+A_2=A_3=(width_1/2-diameter_1/2)*thickness_1
 A_av=6/(3/A_1+1/A_2+1/A_3+1/A_4)
 A_br=diameter_1*thickness_1
 
@@ -20,13 +21,20 @@ A_br=diameter_1*thickness_1
 def P_bry_calculation(thickness_1, diameter_1, width_1, stress_ultimate):
     t_over_D = thickness_1 / diameter_1
     e_over_D = 0.5 * width_1 / diameter_1
-    K_bry =
+    K_bry = graph_reader.get_datapoint(
+        load_case='shear',
+        t_over_D=str(t_over_D),
+        e_over_D=float(e_over_D))
     P_bry = K_bry * A_br * stress_ultimate
     return P_bry
 
 def P_ty_calculation(A_av, A_br, stress_yield):
     Aav_over_Abr = A_av/A_br
-    K_ty =
+
+    K_ty = graph_reader.get_datapoint(
+        load_case='tension',
+        line_number='3',
+        Aav_over_Abr=Aav_over_Abr)
     P_ty = K_ty * A_br * stress_yield
     return P_ty
 
