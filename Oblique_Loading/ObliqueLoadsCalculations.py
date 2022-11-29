9d9f8ff70f3cf776162fa87cec05954357bb6b8e
ThomasClaudel <thomas.claudel@icloud.com>
2022-11-29 17:31:13 +0100

    update1

--- Oblique_Loading/ObliqueLoadsCalculations.py
+++ Oblique_Loading/ObliqueLoadsCalculations.py
@@ -9,16 +9,37 @@ F_z = 570
 F_y = 0.1 * F_z
 
 #Getting the data
-def P_bry_calculation(thickness_1, diameter_1, width_1, stress_ultimate):
+
+#def Moment_calculation(thickness_1, diameter_1, width_1):
+#    MoI_1 =
+def P_bry_calculation(thickness_1, diameter_1, width_1, stress_yield):
     A_av, A_br = AavAbrCalculation(diameter_1, thickness_1, width_1)
     t_over_D = thickness_1 / diameter_1
+    if 0<=t_over_D<0.065:
+        t_over_D = 0.06
+    elif 0.065<=t_over_D<0.09:
+        t_over_D = 0.08
+    elif 0.09<=t_over_D<0.11:
+        t_over_D = 0.10
+    elif 0.11<=t_over_D<0.135:
+        t_over_D = 0.12
+    elif 0.135<=t_over_D<0.175:
+        t_over_D = 0.15
+    elif 0.175<=t_over_D<0.25:
+        t_over_D = 0.2
+    elif 0.25<=t_over_D<0.35:
+        t_over_D = 0.3
+    elif 0.35<=t_over_D<0.5:
+        t_over_D = 0.4
+    elif 0.5<t_over_D:
+        t_over_D = 0.6
     e_over_D = 0.5 * width_1 / diameter_1
     K_bry = graph_reader.get_datapoint(
         load_case='shear',
         t_over_D=str(t_over_D),
         e_over_D=float(e_over_D))
     if K_bry is not None:
-        P_bry = K_bry * A_br * stress_ultimate
+        P_bry = K_bry * A_br * stress_yield
     else:
         P_bry = K_bry
     return P_bry
@@ -43,7 +64,7 @@ def P_ty_calculation(diameter_1, thickness_1, width_1, stress_yield):
         P_ty = K_ty
     return P_ty
 
-def P_y_calculation(diameter_1,thickness_1,width_1,stress_yield,stress_ultimate):
+def P_y_calculation(diameter_1,thickness_1,width_1,stress_yield):
     K_t = graph_reader.get_datapoint(
         load_case='axial',
         line_number_2='1',
@@ -59,11 +80,11 @@ def P_y_calculation(diameter_1,thickness_1,width_1,stress_yield,stress_ultimate)
 
 
 
-def interaction_eq(diameter_1, thickness_1, width_1, stress_yield, stress_ultimate, F_y, F_z):
+def interaction_eq(diameter_1, thickness_1, width_1, stress_yield, F_y, F_z):
     A_av, A_br = AavAbrCalculation(diameter_1, thickness_1, width_1)
-    P_y = P_y_calculation(diameter_1,thickness_1,width_1,stress_yield,stress_ultimate)
+    P_y = P_y_calculation(diameter_1,thickness_1,width_1,stress_yield)
     P_ty = P_ty_calculation(diameter_1, thickness_1, width_1, stress_yield)
-    P_bry  = P_bry_calculation(thickness_1, diameter_1, width_1, stress_ultimate)
+    P_bry  = P_bry_calculation(thickness_1, diameter_1, width_1, stress_yield)
     R_a = F_y / min(P_y, P_bry)
     R_tr = F_z / P_ty
     Interac = R_a ** 1.6 + R_tr ** 1.6
@@ -91,18 +112,19 @@ def best_dimensions(accepted_dimensions):
     return accepted_dimensions[mass_min_index]
 
 accepted_dimensions = []
-for diameter_1 in np.arange(0.001, 0.02, 0.0001):
-    if round(diameter_1%0.005, 4)==0:
-        print("yes")
-        print(best_dimensions(accepted_dimensions))
-    for thickness_1 in np.arange(0.001, 0.02, 0.0001):
-        for width_1 in np.arange(0.001, 0.02, 0.0001):
-            if width_1 > diameter_1:
-                if (P_y_calculation(diameter_1,thickness_1,width_1,stress_yield,stress_ultimate) is not None) and (P_ty_calculation(diameter_1, thickness_1, width_1, stress_yield) is not None) and (P_bry_calculation(thickness_1, diameter_1, width_1, stress_ultimate) is not None):
-                    interac = interaction_eq(diameter_1, thickness_1, width_1, stress_yield, stress_ultimate, F_y, F_z)
-                    if interac >= 1:
-                          mass_1 = calculate_mass(diameter_1,thickness_1,width_1,density)
-                          accepted_dimensions.append([diameter_1, thickness_1, width_1, mass_1, interac])
+all_dimensions = []
+##for diameter_1 in np.arange(0.001, 0.02, 0.0001):
+diameter_1 = 0.0019
+for thickness_1 in np.arange(0.001, 0.02, 0.0001):
+    for width_1 in np.arange(0.001, 0.02, 0.0001):
+        if width_1 > diameter_1:
+            if (P_y_calculation(diameter_1,thickness_1,width_1,stress_yield) is not None) and (P_ty_calculation(diameter_1, thickness_1, width_1, stress_yield) is not None) and (P_bry_calculation(thickness_1, diameter_1, width_1, stress_yield) is not None):
+                interac = interaction_eq(diameter_1, thickness_1, width_1, stress_yield, F_y, F_z)
+                mass_1 = 2* calculate_mass(diameter_1,thickness_1,width_1,density)
+                all_dimensions.append([diameter_1, thickness_1, width_1, mass_1, interac])
+                if interac <= 1:
+                      mass_1 = calculate_mass(diameter_1,thickness_1,width_1,density)
+                      accepted_dimensions.append([diameter_1, thickness_1, width_1, mass_1, interac])
 
 print(best_dimensions(accepted_dimensions))
 
