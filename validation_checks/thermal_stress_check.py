2babb82f8c9a2b15ec6baca98c94d757a1f42d33
JonathanHasASecondName <jonathanpoot3009@gmail.com>
2022-11-30 19:04:42 +0100

    Made start on thermal

--- validation_checks/thermal_stress_check.py
+++ validation_checks/thermal_stress_check.py
@@ -1,10 +1,19 @@
+import numpy as np
+
 from configuration import Configuration
 
+def run_check(configuration: Configuration) -> bool:
 
-class ThermalStressChecker:
-    """
-    Test for thermal stresses
-    """
-    def run_check(self, configuration: Configuration) -> bool:
+    return True
 
-        return True
+def calculate_force_ratio(compliance_a, compliance_b):
+    return compliance_a/(compliance_a+compliance_b)
+
+def calculate_compliance_a(t, youngs_mod, D_fo, D_fi):
+    return 4*t/(youngs_mod*np.pi*(D_fo*D_fo-D_fi*D_fi))  # not using D_fo**2 as D_fo*D_fo is faster
+
+def calculate_compliance_b():
+    pass
+
+def calculate_in_plane_loads(delta_exp_coeff, delta_T, youngs_mod, area_sm, compliance_ratio):
+    return delta_exp_coeff*delta_T*youngs_mod*area_sm*compliance_ratio
