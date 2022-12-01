85dacede97598a1306930c993abd8a34cd515446
ThomasClaudel <thomas.claudel@icloud.com>
2022-12-01 19:11:26 +0100

    update1

--- Oblique_Loading/ObliqueLoadsCalculations.py
+++ Oblique_Loading/ObliqueLoadsCalculations.py
@@ -4,14 +4,34 @@ import numpy as np
 #Input values
 stress_yield = 550*10**6
 stress_ultimate = 620*10**6
+shear_stress_yield = stress_yield/2
 density = 7850
+F_x = 4.5
 F_z = 570
 F_y = 0.1 * F_z
-
+F_y_2 = 0.059
+M_x = 2.79
+M_z = 2.79
 #Getting the data
 
-#def Moment_calculation(thickness_1, diameter_1, width_1):
-#    MoI_1 =
+def Moment_x_calculation(thickness_1, diameter_1, width_1, M_x):
+    tensile_stress_calc = (M_x/(diameter_1))/((width_1-diameter_1)*thickness_1)
+    return tensile_stress_calc
+
+def Force_x_calculation(thickness_1, diameter_1, width_1, F_x):
+    V_x = F_x/4
+    Q = (thickness_1**2)*(width_1-diameter_1)/16
+    I_zz = (((width_1-diameter_1)/2)*thickness_1**3)/12
+    shear_stress_calc = V_x*Q/(I_zz*thickness_1)
+    return shear_stress_calc
+
+def Moment_z_calculation(thickness_1, diameter_1, width_1, M_z, F_y_2):
+    flange_distance = width_1
+    Force = (M_z/flange_distance)/2 + F_y_2/4
+    Area = thickness_1 * ((width_1-diameter_1)/2)
+    tensile_stress_calc = Force/Area
+    return tensile_stress_calc
+
 def P_bry_calculation(thickness_1, diameter_1, width_1, stress_yield):
     A_av, A_br = AavAbrCalculation(diameter_1, thickness_1, width_1)
     t_over_D = thickness_1 / diameter_1
@@ -76,10 +96,6 @@ def P_y_calculation(diameter_1,thickness_1,width_1,stress_yield):
         P_y = K_t
     return P_y
 
-
-
-
-
 def interaction_eq(diameter_1, thickness_1, width_1, stress_yield, F_y, F_z):
     A_av, A_br = AavAbrCalculation(diameter_1, thickness_1, width_1)
     P_y = P_y_calculation(diameter_1,thickness_1,width_1,stress_yield)
@@ -89,8 +105,6 @@ def interaction_eq(diameter_1, thickness_1, width_1, stress_yield, F_y, F_z):
     R_tr = F_z / P_ty
     Interac = R_a ** 1.6 + R_tr ** 1.6
     return Interac
-
-
 
 #mass function
 def calculate_mass(diameter_1,thickness_1,width_1,density):
@@ -113,18 +127,17 @@ def best_dimensions(accepted_dimensions):
 
 accepted_dimensions = []
 all_dimensions = []
-##for diameter_1 in np.arange(0.001, 0.02, 0.0001):
-diameter_1 = 0.0019
-for thickness_1 in np.arange(0.001, 0.02, 0.0001):
-    for width_1 in np.arange(0.001, 0.02, 0.0001):
-        if width_1 > diameter_1:
-            if (P_y_calculation(diameter_1,thickness_1,width_1,stress_yield) is not None) and (P_ty_calculation(diameter_1, thickness_1, width_1, stress_yield) is not None) and (P_bry_calculation(thickness_1, diameter_1, width_1, stress_yield) is not None):
-                interac = interaction_eq(diameter_1, thickness_1, width_1, stress_yield, F_y, F_z)
-                mass_1 = 2* calculate_mass(diameter_1,thickness_1,width_1,density)
-                all_dimensions.append([diameter_1, thickness_1, width_1, mass_1, interac])
-                if interac <= 1:
-                      mass_1 = calculate_mass(diameter_1,thickness_1,width_1,density)
-                      accepted_dimensions.append([diameter_1, thickness_1, width_1, mass_1, interac])
+for diameter_1 in np.arange(0.001, 0.02, 0.0001):
+    for thickness_1 in np.arange(0.001, 0.02, 0.0001):
+        for width_1 in np.arange(0.001, 0.02, 0.0001):
+            if width_1 > diameter_1:
+                if (P_y_calculation(diameter_1,thickness_1,width_1,stress_yield) is not None) and (P_ty_calculation(diameter_1, thickness_1, width_1, stress_yield) is not None) and (P_bry_calculation(thickness_1, diameter_1, width_1, stress_yield) is not None):
+                    interac = interaction_eq(diameter_1, thickness_1, width_1, stress_yield, F_y, F_z)
+                    mass_1 = 2* calculate_mass(diameter_1,thickness_1,width_1,density)
+                    all_dimensions.append([diameter_1, thickness_1, width_1, mass_1, interac])
+                    if interac <= 1 and Moment_x_calculation(thickness_1, diameter_1, width_1, M_x)<=stress_yield and Force_x_calculation(thickness_1, diameter_1, width_1, F_x)<=shear_stress_yield and Moment_z_calculation(thickness_1, diameter_1, width_1, M_z, F_y_2)<= stress_yield:
+                        mass_1 = calculate_mass(diameter_1,thickness_1,width_1,density)
+                        accepted_dimensions.append([diameter_1, thickness_1, width_1, mass_1, interac, Moment_x_calculation(thickness_1, diameter_1, width_1, M_x), Force_x_calculation(thickness_1, diameter_1, width_1, F_x),Moment_z_calculation(thickness_1, diameter_1, width_1, M_z, F_y_2)])
 
 print(best_dimensions(accepted_dimensions))
 
